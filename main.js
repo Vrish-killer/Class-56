478f7e7a84d73a2a8eeb20b13950627046272876
Vrish-killer <84429413+Vrish-killer@users.noreply.github.com>
2021-05-19 08:26:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor(){
+document.getElementById("H1").style.color="green";     
+document.getElementById("H2").style.color="blue";   
+}
+function Germen(){
+document.getElementById("dog").src="Dog 1.jpg";    
+}
